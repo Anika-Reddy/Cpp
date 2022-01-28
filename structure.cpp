33c888045e757d568f65d6b765969ad5f24ff456
Anika-Reddy <95438782+Anika-Reddy@users.noreply.github.com>
2022-01-28 13:34:00 +0530

    C++ program to declare struct
    Initialize and display contents of member variables.

---
+++ structure.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+ struct collage_info{
+
+     char college_name[15];
+     char college_code[10];
+     char dept[50];
+      
+      int intake;
+
+ };
+
+ int main(){
+
+     struct college_info college;
+     
+
+
+ cout<<"Enter the collage information \n";
+ cout<<"Name of the collage :";
+ cin>>college.college_name;
+ cout<<"College Code :";
+ cin>>college.college_code;
+ cout<<"Department:";
+ cin>>collegek.debt;
+ cout<<"Department in-take :";
+ cin>>collge.intake;
+ cout<<"\nCollege Information:";
+ cout<<"Name of the college : "<<college.college_code;
+ cout<<"\nCollege University Code :"<<college.college_code;
+ cout<<"\nName of the Department :"<<college.dept;
+ cout<<"\nThe department of "<<college.dept<<" has in-tale:"<<college.intake;
+
+  return 0 ;
+ }
